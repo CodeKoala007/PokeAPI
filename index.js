6a74341f2258034f26c61cc2c7a9394428d87d04
Edgar Agudelo <edgaragudelo08@gmail.com>
2025-03-30 15:14:17 -0500

    Refactor Pokemon fetcher functionality and enhance UI layout

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 //pokeapi.co
 // https://pokeapi.co/api/v2/pokemon/pikachu
-
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-.then((response))
-.catch(error => console.error('Error:', error));
+fetchPokemon();
 
 async function fetchPokemon() {
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('Error:', error);
+  try {
+    const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    if (!response.ok) {
+      throw new Error('No se pudo encontrar el Pokémon. Por favor, verifica el nombre e intenta nuevamente.');
     }
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById('pokemonSprite');
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = 'block';
+    imgElement.alt = pokemonName;
+    const pokemonHeight = data.height;
+    const pokemonWeight = data.weight;
+    const pokemonTypes = data.types.map((typeInfo) => typeInfo.type.name).join(', ');
+    const pokemonAbilities = data.abilities.map((abilityInfo) => abilityInfo.ability.name).join(', ');
+
+    const detailsElement = document.getElementById('pokemonDetails');
+    detailsElement.innerHTML = `
+            <strong>Height:</strong> ${pokemonHeight} decimetres<br>
+            <strong>Weight:</strong> ${pokemonWeight} hectograms<br>
+            <strong>Types:</strong> ${pokemonTypes}<br>
+            <strong>Abilities:</strong> ${pokemonAbilities}
+        `;
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
